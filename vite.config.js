d7a7dea1bb3c775fc7232c7eaa29fcb37540542a
blakcoder2017 <sherifdata@gmail.com>
2025-04-06 20:14:12 +0000

    final settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: "/shoppingreact",
-  base: "https://blakcoder2017.github.io/e-plantShopping/",
+  //base: "https://blakcoder2017.github.io/e-plantShopping/",
   plugins: [react()],
+  base: "/e-plantShopping/",
 })

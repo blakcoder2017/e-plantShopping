8ed57eaced749319b87226dad253849da121c008
blakcoder2017 <sherifdata@gmail.com>
2025-04-06 16:02:25 +0000

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  //base: "/shoppingreact",
+  base: "https://blakcoder2017.github.io/e-plantShopping/",
   plugins: [react()],
 })
